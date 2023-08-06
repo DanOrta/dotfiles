d3bbb5d937fffab63670f9e308411aa7c3ab6a1f
Daniel <daniel@kali-desktop>
2023-08-06 16:40:02 -0600

    Added a bash script to update software packages.

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+userName=$(whoami)
+if [ $userName = "root" ];then
+	echo -e "Updating packages list..."
+	apt-get update
+	echo -e "\nUpgrading packages..."
+	apt-get full-upgrade --fix-missing -y
+	echo -e "\nRemoving unnecessary packages..."
+	apt autoremove -y
+else
+	echo -n -e "You are not root! Please use sudo to run this script!!\a"
+fi
