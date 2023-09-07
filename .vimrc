c942a964e30d10a01538e92b448537488957e27f
Daniel <kali@kali-desktop>
2023-09-06 22:58:00 -0600

    .vimrc: added new key mapping and removed tab expand. RenameFilesWithSHAsum.sh: (1) fixed the issue #3 in which the script didn't recognize file with spaces. (2) Improved the examples with the new implementation (derived from the fix). (3) Added colors to some prompts/messages. UpdatePackages.sh: added colors to some prompts/messages and added the 'dist-upgrade' instruction.

--- .vimrc
+++ .vimrc
@@ -7,7 +7,6 @@ filetype indent on          " Enable filetype-specific indenting
 set autoindent              " smartindent wasn't so smart after all
 set autoread                " Enable auto-read files
 set clipboard=unnamedplus
-set expandtab               " Use spaces instead of tabs
 set guioptions=aAace        " Don't show scrollbar
 set hlsearch
 set incsearch               " Set incremental search
@@ -33,10 +32,13 @@ inoremap ' ''<Left>
 inoremap ¿ ¿?<Left>
 inoremap ¡ ¡!<Left>
 
+" LaTeX commands
+autocmd FileType tex map <c-m> :wa<CR>:make<CR>
+
 " Map Ctrl+S to save all files
-map <c-s> <Esc>:wa<Enter>
+map <c-s> <Esc>:wa<CR>
 " Map Ctrl+S in insert mode to save a file
-inoremap <c-s> <Esc>:w<Enter>
+inoremap <c-s> <Esc>:w<CR>
 " Map Ctrl+Z 
 map <c-z> u
 " Map Ctrl+Z in insert mode
@@ -46,7 +48,7 @@ map <Esc><Esc> :qa!<Enter>
 " Go to he beginning of the document
 map <Home><Home> <c-Home>
 " Go to the end of the file
-map <End><End> <c-End>
+map <End><End> GA
 " Go to the end of the file and add a new line
 map <End>+ <c-End>o
 
@@ -59,6 +61,8 @@ map -- :q!<Enter>
 map <c-Right> gt
 " Move to the tab to the left
 map <c-Left> gT
+" Open the terminal in a tab at the bottom
+map tt :bo term<CR>
 
 " C PROGRAMMING LANGUAGE
 " Single-line C comment
--- RenameFilesWithSHAsum.sh
+++ RenameFilesWithSHAsum.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 # We need to check if "dmenu" is installed
-VAR=$(apt list --installed | grep "suckless-tools" | cut -d "/" -f1)
-if [ $VAR = "" ];then
-	echo "dmenu is not installed!"
+RESET='\033[00m'
+RED='\033[01;31m'
+VAR=$(whereis dmenu)
+if [ "$VAR" == "dmenu:" ];then
+	echo -e $RED"dmenu is not installed!"$RESET
 else
-	HASH=$(echo -e "sha1sum\nsha224sum\nsha256sum\nsha384sum\nsha512sum\nshasum" | dmenu -l 6)
+	HASH=$(echo -e "md5sum\nsha1sum\nsha224sum\nsha256sum\nsha384sum\nsha512sum\nshasum" | dmenu -l 6)
 	uplo=$(echo -e "UPPERCASE\nlowercase" | dmenu -l 2)
 	if [ $uplo = "UPPERCASE" ];then
-		for i in *.*; do newName="$($HASH -b $i | cut -d " " -f1).$(echo $i | cut -d "." -f2)"; newName=$(echo -n $newName | tr [:lower:] [:upper:]);  mv $i $newName -vn; done
+        for i in *.*;do EXT=$(echo $i | cut -d "." -f2);SHA=$($HASH -b "$i" | cut -d " " -f1);NEWNAME=$(echo "$SHA.$EXT" | tr [:lower:] [:upper:]);mv "$i" $NEWNAME -vn;done
 	else
-		for i in *.*; do newName="$($sha -b $i | cut -d " " -f1).$(echo $i | cut -d "." -f2)"; newName=$(echo -n $newName | tr [:upper:] [:lower:]);  mv $i $newName -vn; done
+        for i in *.*;do EXT=$(echo $i | cut -d "." -f2);SHA=$($HASH -b "$i" | cut -d " " -f1);NEWNAME=$(echo "$SHA.$EXT" | tr [:upper:] [:lower:]);mv "$i" $NEWNAME -vn;done
 	fi
 fi
-#for i in *.*; do newName="$(sha1sum -b $i | cut -d " " -f1).$(echo $i | cut -d "." -f2)"; newName=$(echo -n $newName | tr [:lower:] [:upper:]);  mv $i $newName -vn; done
+# Alternative Options:
+#
 # Generate a report of all the files that were renamed!
-# for i in *.*; do newName="$(sha1sum $i | cut -d " " -f1).$(echo $i | cut -d "." -f2)"; newName=$(echo -n $newName | tr [:lower:] [:upper:]);  mv $i $newName -vn >> REPORT.LOG; done
+# for i in *.*;do EXT=$(echo $i | cut -d "." -f2);SHA=$($HASH -b "$i" | cut -d " " -f1);NEWNAME=$(echo "$SHA.$EXT" | tr [:lower:] [:upper:]);mv "$i" $NEWNAME -vn >> REPORT.LOG;done
+#
 # Generate a CSV file with the sha-sum of all the files
-# for i in *.*; do j="$(sha256sum $i | cut -d " " -f1), $i"; echo $j; done > sha256out.csv
+# for i in *.*;do SHA="$(sha1sum -b ${i} | cut -d " " -f1), ${i}";echo "$SHA";done > sha256sum_ouput.csv
